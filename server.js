c7ff7c469f678a9215ab3e5c079369169ac4ffae
Karl Shiu <bkshiu1@myseneca.ca>
2025-07-04 23:39:37 -0400

    Update for Assignment 4

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 /********************************************************************************
-*  WEB322 – Assignment 03
+*  WEB322 – Assignment 04 
 * 
 *  I declare that this assignment is my own work in accordance with Seneca's
 *  Academic Integrity Policy:
 * 
 *  https://www.senecacollege.ca/about/policies/academic-integrity-policy.html
 * 
-*  Name: Karl Shiu          Student ID: 131531246       Date: 2025-06-13
+*  Name: Karl Shiu  Student ID: 131531246  Date: 4/7/2025 
 *
 *  Published URL: https://assignment-3-sty4.onrender.com
-
 *
 ********************************************************************************/
 
@@ -20,41 +19,75 @@ const HTTP_PORT = process.env.PORT || 8080;
 
 const projectData = require("./modules/projects");
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, "views"));
+
 app.use(express.static("public"));
 
 projectData.initialize().then(() => {
+
   app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/home.html"));
+    res.render("home", { page: "/" });
   });
+
   app.get("/about", (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/about.html"));
+    res.render("about", { page: "/about" });
   });
+
   app.get("/solutions/projects", (req, res) => {
     const sector = req.query.sector;
 
-    if (sector) {
-      projectData.getProjectsBySector(sector)
-        .then(data => res.json(data))
-        .catch(err => res.status(404).send(err));
-    } else {
-      projectData.getAllProjects()
-        .then(data => res.json(data))
-        .catch(err => res.status(404).send(err));
-    }
-  });
-  app.get("/solutions/projects/:id", (req, res) => {
-    const projectId = parseInt(req.params.id);
+    const fetch = sector
+      ? projectData.getProjectsBySector(sector)
+      : projectData.getAllProjects();
 
-    projectData.getProjectById(projectId)
-      .then(data => res.json(data))
-      .catch(err => res.status(404).send(err));
+    fetch
+      .then(data => {
+        res.render("projects", {
+          page: "/solutions/projects",
+          data
+        });
+      })
+      .catch(err => {
+        res.status(404).render("404", {
+          page: "/solutions/projects",
+          message: "No projects found."
+        });
+      });
   });
+
+  //changed
+app.get("/solutions/projects/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  projectData.getProjectById(id)
+    .then(project => {
+res.render("project", {
+  page: "/solutions/projects",
+  data: project
+});
+
+    })
+    .catch(err => {
+      res.status(404).render("404", {
+        page: "/solutions/projects",
+        message: "Project not found."
+      });
+    });
+});
+
+
   app.use((req, res) => {
-    res.status(404).sendFile(path.join(__dirname, "/views/404.html"));
+    res.status(404).render("404", {
+      page: "",
+      message: "Sorry, the page you're looking for doesn't exist."
+    });
   });
+
   app.listen(HTTP_PORT, () => {
     console.log(`Server listening on port ${HTTP_PORT}`);
   });
+
 }).catch(err => {
   console.error("Failed to start server:", err);
 });
